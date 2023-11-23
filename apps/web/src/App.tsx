6b3b0156888b186ab45c4959305b06c081f9ec22
Aqudi <gjdigj@naver.com>
2023-11-23 22:01:18 +0900

    feat(web): :sparkles: show visit count

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "ui";
 import "./App.css";
+import type { AxiosResponse } from "axios";
+import client from "./client";
 
 function App(): JSX.Element {
+  const [visits, setVisits] = useState("");
+
+  const visit = (): void => {
+    const res = client.get("http://localhost:4000");
+    res
+      .then((v: AxiosResponse<string>) => {
+        setVisits(v.data);
+      })
+      .catch(() => {
+        setVisits("");
+      });
+  };
+
+  useEffect(() => {
+    visit();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="header">
-          Web
+          {visits}
           <div className="Turborepo">Turborepo Example</div>
         </h1>
+        <button onClick={visit} type="button">
+          방문
+        </button>
         <div>
           <Link className="App-link" href="https://turbo.build/repo">
             Turborepo Docs
